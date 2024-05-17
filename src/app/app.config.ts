77de013ed5e2a1091f897a6f4ce0e268d3e3b482
Jesse Zomer <jessezomer@hotmail.com>
2024-05-17 13:26:38 +0200

    add bugsnag

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: ErrorHandler,
+      useFactory() {
+        return new BugsnagErrorHandler();
+      },
+    },
+  ],
 };
